ddb8da5b94a20721231d632284ce27b17700f9b5
Jinsei-Tsunda <66623654+Jinsei-Tsunda@users.noreply.github.com>
2020-11-10 15:23:50 +0800

    api fixed

--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -2,12 +2,16 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 
-const products = require(__dirname + '/products')
-
-app.use('/cart', products)
+const db = require(__dirname + '/../db_connect')
 
 router.get('/', (req, res) => {
   res.send('123')
+})
+
+router.get('/cart', (req, res) => {
+  db.query('SELECT * FROM tsao_products').then(([results]) => {
+    res.json(results)
+  })
 })
 
 module.exports = router
